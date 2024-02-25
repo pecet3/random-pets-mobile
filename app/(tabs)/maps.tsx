db3148daaeae4b42ec5b287f2b37dd628c56b00b
pecet <pecet3107@gmail.com>
2024-02-25 03:05:22 +0100

    location and maps works!

--- app/(tabs)/maps.tsx
+++ app/(tabs)/maps.tsx
@@ -1,24 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-import Device from 'expo-device';
-import * as Location from 'expo-localization';
+import { useNavigation } from 'expo-router';
+import React, { useState, useEffect, useRef } from 'react';
+import { Platform, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const initialRegion = {
+  latitude: 54.518333,
+  longitude: 18.550833,
+  latitudeDelta: 0.005,
+  longitudeDelta: 0.005,
+};
 
 export default function MapsView() {
+  const mapRef = useRef<any>(null)
+  const nav = useNavigation()
+
+  useEffect(() => {
+    nav.setOptions({
+      headerRight: () => {
+
+      }
+    })
+  }, [])
+
+  const focusMap = () => {
+    const policeRegion = {
+      latitude: 53.56153425831027,
+      longitude: 21.01730780587174,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005,
+    };
+    // mapRef.current?.animateToRegion(policeRegion)
+    mapRef.current?.animateCamera({ center: policeRegion }, { duration: 3000 })
+
+  }
+
+  const onRegionChange = (region: Region) => {
+
+  }
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
-      if (Platform.OS === 'android' && !Device.isDevice) {
-        return;
-      }
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
+
         return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      console.log(location.coords.latitude, location.coords.longitude)
+      setLocation(location as any);
     })();
   }, []);
 
@@ -28,10 +62,23 @@ export default function MapsView() {
   } else if (location) {
     text = JSON.stringify(location);
   }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
+      <TouchableOpacity onPress={focusMap}>
+        <View style={{ padding: 10 }}>
+          <Text>Focus</Text>
+        </View>
+      </TouchableOpacity>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={initialRegion}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        ref={mapRef}
+        showsPointsOfInterest={false}
+        mapType={'satellite'}
+      />
     </View>
   );
 }
@@ -41,10 +88,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    padding: 0,
   },
   paragraph: {
     fontSize: 18,
     textAlign: 'center',
   },
+  map: {
+    width: '100%',
+    height: '95%',
+  }
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,7 +48,7 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'light' ? DefaultTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
